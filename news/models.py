8fb985e2b5647197d3d004893f484c6a8f6bbf99
brayomumo <brayomumo5@gmail.com>
2019-08-18 23:48:31 +0300

    add Query-database functions

--- news/models.py
+++ news/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
 class Editor(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10, blank = True)
 
     def __str__(self):
         return self.first_name
 
     def save_editor(self):
         self.save()
-    
 
     class Meta:
         ordering = ['first_name']
@@ -42,3 +43,14 @@ class Article(models.Model):
 
     def delete_Article(self):
         self.delete()
+
+    @classmethod
+    def todays_news(cls):
+        today = dt.date.today()
+        news = cls.objects.filter(pub_date__date = today)
+        return news
+
+    @classmethod
+    def days_news(cls, date):
+        news = cls.objects.filter(pub_date__date = date)
+        return news
