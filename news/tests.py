15a7359a475926fbeadcedd58e5ce86b01cc35d8
brayomumo <brayomumo5@gmail.com>
2019-08-18 23:50:41 +0300

    System testing

--- news/tests.py
+++ news/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import Article,Editor,Tags
+import datetime as dt
 
 # Create your tests here.
 class EditorTestCase(TestCase):
@@ -21,19 +22,38 @@ class EditorTestCase(TestCase):
     #     self.assertTrue(len(editors) > 0)
 
 
-# class ArticleTestCase(TestCase):
-#     def setUp(self):
-#         self.article1 = Article(title = 'news1', post = 'Making it happen at Moringa Get informed')
+class ArticleTestClass(TestCase):
+    
+    def setUp(self):
+        # Creating a new editor and saving it
+        self.james= Editor(first_name = 'James', last_name ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
 
-#     #Test instance 
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.article1, Article))
+        # Creating a new tag and saving it
+        self.new_tag = Tags(name = 'testing')
+        self.new_tag.save()
 
-#     def test_save_method(self):
-#         self.article1.save_article()
-#         article = Article.objects.all()
-#         self.assertTrue(len(article) > 0)
+        self.new_article= Article(title = 'Test Article',post = 'This is a random test Post',editor = self.james)
+        self.new_article.save()
 
+        self.new_article.tags.add(self.new_tag)
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news)>0)
+
+    def test_get_news_today(self):
+        test_date = '2019-08-18'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+        self.assertTrue(len(news_by_date) == 0)
+       
+
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
 
 class TagsTestCase(TestCase):
 
