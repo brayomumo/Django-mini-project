62ed3dac6c1b4a296fd6172f23ffa41cd1387c3c
brayomumo <brayomumo5@gmail.com>
2019-08-18 15:58:15 +0300

    Set templates

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/smoucha/Desktop/python/Django/The-Moringa-Tribune/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
