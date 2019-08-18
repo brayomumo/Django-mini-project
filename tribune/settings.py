6a2060511de32e09ee0d6cf416e20123571620a5
brayomumo <brayomumo5@gmail.com>
2019-08-18 16:21:38 +0300

    set url for static folder in the settings.py

--- tribune/settings.py
+++ tribune/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap3',
     'news.apps.NewsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR, "static"),
+]
